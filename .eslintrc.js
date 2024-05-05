96740bb4a8531de2a5a44139ec3914ea574a539a
andhikay24 <andhika.perdana@btpn.com>
2024-05-05 11:07:07 +0700

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,11 +19,13 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': 'off',
     'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
     'comma-dangle': 'off',
     'import/prefer-default-export': 'off',
     'react/function-component-definition': 'off',
     'react/jsx-props-no-spreading': 'off',
     'class-methods-use-this': 'off',
-    'no-undef': 'off'
+    'no-undef': 'off',
+    'no-unused-vars': 'off'
   },
 };
